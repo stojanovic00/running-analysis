be5459d21f4429fe7694baf32079ff58189f353e
Aleksandar <al.stojanovic0@gmail.com>
2023-12-29 15:48:40 +0100

    Added volume for hive metstore

--- stream/start_consumer.sh
+++ stream/start_consumer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec -it  kafka1 kafka-console-consumer --bootstrap-server kafka1:9092 --topic RUN_STATS --from-beginning
+docker exec -it  kafka1 kafka-console-consumer --bootstrap-server "kafka1:19092,kafka2:19093,kafka3:19094"  --topic RUN_STATS --from-beginning
 
--- stream/start_producer.sh
+++ stream/start_producer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-docker exec -it  kafka1 kafka-console-producer --broker-list kafka1:9092 --topic RUN_STATS
+docker exec -it  kafka1 kafka-console-producer --broker-list "kafka1:19092,kafka2:19093,kafka3:19094"  --topic RUN_STATS
 
