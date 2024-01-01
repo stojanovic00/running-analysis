960398665d2ef3354f449ec0506054e3f8dad3d0
Aleksandar <al.stojanovic0@gmail.com>
2024-01-01 18:54:39 +0100

    Multithreaded consumer and mongosh scripts

---
+++ infrastructure/clear-streams-db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mongosh mongodb://mongo:mongo@localhost:27017/ < clear-commands
+
--- stream/consumer/main.go
+++ stream/consumer/main.go
@@ -47,26 +47,31 @@ func main() {
 		if err == nil {
 			switch *msg.TopicPartition.Topic {
 			case "speed":
+				go func(currTime time.Time) {
+					document := map[string]interface{}{
+						"time":  currTime,
+						"speed": string(msg.Value),
+					}
 
-				document := map[string]interface{}{
-					"time":  time.Now().String(),
-					"speed": string(msg.Value),
-				}
-
-				_, err = speedCol.InsertOne(context.Background(), document)
-				if err != nil {
-					fmt.Println("Error writing to speed")
-				}
+					_, err = speedCol.InsertOne(context.Background(), document)
+					log.Println("Wrote to speed")
+					if err != nil {
+						fmt.Println("Error writing to speed")
+					}
+				}(time.Now().Add(time.Hour))
 			case "heart_rate":
-				document := map[string]interface{}{
-					"time":      time.Now().String(),
-					"hear_rate": string(msg.Value),
-				}
+				go func(currTime time.Time) {
+					document := map[string]interface{}{
+						"time":       currTime,
+						"heart_rate": string(msg.Value),
+					}
 
-				_, err = heartRateCol.InsertOne(context.Background(), document)
-				if err != nil {
-					fmt.Println("Error writing to hear_rate")
-				}
+					_, err = heartRateCol.InsertOne(context.Background(), document)
+					log.Println("Wrote to heart_rate")
+					if err != nil {
+						fmt.Println("Error writing to heart_rate")
+					}
+				}(time.Now().Add(time.Hour))
 			}
 		} else if !err.(kafka.Error).IsTimeout() {
 			// The client will automatically try to recover from all errors.
