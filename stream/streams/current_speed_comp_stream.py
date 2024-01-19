3f7ebf7faca6d8a08b1e926af3fc2e5d789378ee
Aleksandar <al.stojanovic0@gmail.com>
2024-01-19 05:20:12 +0100

    Avg and current speed comparison

--- stream/streams/current_speed_comp_stream.py
+++ stream/streams/current_speed_comp_stream.py
@@ -1,7 +1,7 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, round, window, avg, current_timestamp, coalesce, lit, expr, date_format, \
-    from_unixtime, to_date
+    from_unixtime, to_date, split, to_timestamp, concat
 
 conf = SparkConf() \
     .setAppName("current-speed-comp-streamer") \
@@ -13,43 +13,56 @@ spark = SparkSession.builder \
     .config(conf=conf) \
     .getOrCreate()
 
+# SPEED
 kafka_speed_input_options = {
     "kafka.bootstrap.servers": "kafka1:19092,kafka2:19093,kafka3:19094",
     "subscribe": "speed"
 }
 
-speed_df = spark.readStream \
+df = spark.readStream \
     .format("kafka") \
     .options(**kafka_speed_input_options) \
     .option("failOnDataLoss", "false") \
     .load() \
-    .withColumn("speed", col("value").cast("string")) \
-    .withColumn("speed", col("speed").cast("float")) \
-    .withColumn( 'formatted_timestamp', to_date(col('timestamp'), 'yyyy-MM-dd HH:mm:ss'))
 
+speed_df = df.select(
+    split(col("value"), ',')[0].alias("time_str"),
+    split(col("value"), ',')[1].alias("speed_str")
+)
+
+speed_df = speed_df.withColumn("time", to_timestamp(col("time_str")))
+speed_df = speed_df.withColumn("speed", col("speed_str").cast("float"))
+
+# AVG SPEED
 kafka_avg_speed_input_options = {
     "kafka.bootstrap.servers": "kafka1:19092,kafka2:19093,kafka3:19094",
     "subscribe": "avg_speed"
 }
 
-avg_speed_df = spark.readStream \
+df = spark.readStream \
     .format("kafka") \
     .options(**kafka_avg_speed_input_options) \
     .option("failOnDataLoss", "false") \
     .load() \
-    .withColumn("avg_speed", col("value").cast("string")) \
-    .withColumn("avg_speed", col("avg_speed").cast("float")) \
-    .withColumn( 'formatted_timestamp', to_date(col('timestamp'), 'yyyy-MM-dd HH:mm:ss'))
 
-joined_df = speed_df.join(avg_speed_df, "formatted_timestamp").limit(1)
+avg_speed_df = df.select(
+    split(col("value"), ',')[0].alias("time_str"),
+    split(col("value"), ',')[1].alias("avg_speed_str")
+)
+
+avg_speed_df = avg_speed_df.withColumn("time", to_timestamp(col("time_str")))
+avg_speed_df = avg_speed_df.withColumn("avg_speed", col("avg_speed_str").cast("float"))
+
+joined_df = speed_df.join(avg_speed_df, "time")
 
 # Compare speed and avg_speed, then determine status
 status_df = joined_df.withColumn(
-    "value",
+    "comparison",
     expr("CASE WHEN speed > avg_speed THEN 'faster' ELSE 'slower' END")
 )
 
-status_df.withColumn("timestamp", avg_speed_df["timestamp"])
+status_df = status_df.withColumn("time_string", date_format("time", "yyyy-MM-dd HH:mm:ss"))
+status_df = status_df.withColumn("value", concat(col("time_string"), lit(","), col("comparison")))
 
 write_kafka_params = {
     "kafka.bootstrap.servers": "kafka1:19092,kafka2:19093,kafka3:19094",
